4c9b20f3d342f36cfa4fe97cc332a4bda6f19493
Ignacio Irusta <ignacio_r99@hotmail.com>
2021-12-01 22:28:48 +0100

    Completando el carrito de compra

--- index.js
+++ index.js
@@ -51,208 +51,122 @@ let botonesCompra = document.querySelectorAll(".botonCompra");
 let carrito = [];
 
 
-
 for( let boton of botonesCompra){
-
     boton.addEventListener("click" , agregarCarrito);
-
 }
 
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClick);
 
-console.log(botonesCompra);
-
-
+const carritoCartCONTAINER = document.querySelector('.shoppingCartItemsContainer');
 
 function agregarCarrito(e){
 
-    console.log(e.target);
-    let hijo = e.target;
-    let padre = hijo.parentNode.parentNode;
-    let titulo = padre.querySelector("h5").textContent;
-    let parraf = padre.querySelector("p").textContent;
-    let imagen = padre.querySelector("img").src;
+    const button = e.target;
+    const card = button.closest('.card');
+    
+    const cardTitulo = card.querySelector('.card-title').textContent;
+    const cardPrecio = card.querySelector('.card-text').textContent;
+    const cardIMG = card.querySelector('.card-img-top').src;
 
-
-    let producto = {
-        nombre: titulo,
-        desc:parraf,
-        img: imagen
-    };
-
-
-    carrito.push(producto)
-    console.log(padre);
-    console.log(titulo);
+    addCardCarritoCompra(cardTitulo, cardPrecio, cardIMG);
 }
 
-/*
+//Funcion del carrito
 
-let arrays = [];
-let contred = 0;
-let contcall = 0;
-let contcsgo = 0;
-let contrust = 0;
-let contforest = 0;
-let contday = 0;
-let contfifa = 0;
-let contnba = 0;
-let conthorizon = 0;
-let totalC = 0;
-let totalJ = 0;
-let cof;
+function addCardCarritoCompra(cardTitulo, cardPrecio, cardIMG){
 
-class videojuego{
+  const elementsTitulo = carritoCartCONTAINER.getElementsByClassName('.shoppingCartItemTitle');
 
-    constructor(precio){
+  // ACA tendria que hacer que en el carrito solo se vea uno cuando repito producto, pero sigue mostrandome mas de uno
+  for(let i = 0; i < elementsTitulo.length; i++){
+    if(elementsTitulo[i].innetText === cardTitulo){
 
-    this.precio = precio;
+      let element = elementsTitulo[i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity');
 
-    }
-
-    preciofinal(){
-
-        let iva = this.precio * 0.21;
-        return this.precio + iva;
-
-    }
-}
-  console.log("Tenemos 3 generos de juegos: Shooter, Survival y Deportivo.");
-  let catalogo = prompt(
-      "Ingrese el genero para ver el catalogo o ESC para salir: "
-  );
-
-  segunda();
-
-  function segunda() {
-    while (catalogo != "ESC") {
-      if (catalogo == "Shooter") {
-        console.log("En la categoria de Shooter tenemos: ");
-        console.log("Red Dead Redemption: ARS$ 2,499");
-        console.log("Call of Duty: Black Ops 3: ARS$ 999");
-        console.log("CS:GO: ARS$ 759");
-        let eleccion = prompt(
-          "Elija un juego para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-        );
-
-        while (eleccion != "ESC") {
-          if (eleccion == "Red Dead Redemption") {
-            contred = contred + 1;
-            array2 = "Red Dead Redemption";
-            arrays.push(array2);
-            totalC = totalC + 2499;
-          } else if (eleccion == "Call of Duty: Black Ops 3") {
-            contcall = contcall + 1;
-            array2 = "Call of Duty: Black Ops 3";
-            arrays.push(array2);
-            totalC = totalC + 999;
-          } else if (eleccion == "CS:GO") {
-            contcsgo = contcsgo + 1;
-            array2 = "CS:GO";
-            arrays.push(array2);
-            totalC = totalC + 759;
-          } else {
-            alert(
-              "Ingrese el nombre del juego correctamente. (Importante, respete mayusculas)"
-            );
-          }
-          eleccion = prompt(
-            "Elija un juego para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-          );
-        }
-      } else if (catalogo == "Survival") {
-        console.log("En la categoria de Survival tenemos: ");
-        console.log("Rust: ARS$ 2,440");
-        console.log("The Forest: ARS$ 224");
-        console.log("Day Z: ARS$ 1,999");
-        let eleccion2 = prompt(
-          "Elija uno para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-        );
-
-        while (eleccion2 != "ESC") {
-          if (eleccion2 == "Rust") {
-            contrust = contrust + 1;
-            array2 = "Rust";
-            arrays.push(array2);
-            totalC = totalC + 2440;
-          } else if (eleccion2 == "The Forest") {
-            contforest = contforest + 1;
-            array2 = "The Forest";
-            arrays.push(array2);
-            totalC = totalC + 224;
-          } else if (eleccion2 == "Day Z") {
-            contday = contday + 1;
-            array2 = "Day Z";
-            arrays.push(array2);
-            totalC = totalC + 1999;
-          } else {
-            alert(
-              "Ingrese el nombre del juego correctamente. (Importante, respete mayusculas)"
-            );
-          }
-          eleccion2 = prompt(
-            "Elija un juego para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-          );
-        }
-      } else if (catalogo == "Deportivo") {
-        console.log("En la categoria de deportes tenemos: ");
-        console.log("Fifa 2022: ARS$ 5,499");
-        console.log("NBA2K 2022: ARS$ 4,019");
-        console.log("Forza Horizon 5: ARS$ 3,599");
-        let eleccion3 = prompt(
-          "Elija uno para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-        );
-
-        while (eleccion3 != "ESC") {
-          if (eleccion3 == "Fifa 2022") {
-            contfifa = contfifa + 1;
-            array2 = "Fifa 2022";
-            arrays.push(array2);
-            totalC = totalC + 5499;
-          } else if (eleccion3 == "NBA2k 2022") {
-            contnba = contnba + 1;
-            array2 = "NBA2k 2022";
-            arrays.push(array2);
-            totalC = totalC + 4019;
-          } else if (eleccion3 == "Forza Horizon 5") {
-            conthorizon = conthorizon + 1;
-            array2 = "Forza Horizon 5";
-            arrays.push(array2);
-            totalC = totalC + 3599;
-          } else {
-            alert(
-              "Ingrese el nombre del juego correctamente. (Importante, respete mayusculas)"
-            );
-          }
-          eleccion3 = prompt(
-            "Elija un juego para agregar al carro ingresando su nombre o ESC para vovler a la seleccion de genero."
-          );
-        }
-      } else {
-        alert("Ingreso mal la catergoria");
-      }
-      catalogo = prompt(
-        "Ingrese el genero para ver el catalogo o ESC para salir: "
-      );
+      element.value++;
+      actualizarTotal();
+      return;
     }
   }
+  //--
 
-let importeiva = new videojuego(totalC);*/
+  const carritoCartROW = document.createElement('div');
+  const carritoCartcontent = `
+  <div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${cardIMG} width="60" height="80" class="shopping-cart-image">
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${cardTitulo}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${cardPrecio}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>`;
+    carritoCartROW.innerHTML = carritoCartcontent;
+    carritoCartCONTAINER.append(carritoCartROW);
 
-/*tercera();
+    carritoCartROW.querySelector('.buttonDelete').addEventListener('click', removecarrito);
 
-function tercera() {
-  totalJ =
-    contcall +
-    contcsgo +
-    contday +
-    contfifa +
-    contforest +
-    conthorizon +
-    contnba +
-    contred +
-    contrust;
-  console.log("Le informamos que la cantidad de juegos en su carrito es " + totalJ);
-  console.log("Los juegos en su carritos son los siguientes: " + arrays.sort());
-  console.log("El importe a pagar mas el IVA(21%) es: " + importeiva.preciofinal());
-  console.log("Gracias por su compra.");
-}*/
+    carritoCartROW.querySelector('.shoppingCartItemQuantity').addEventListener('change',Quantitycambio);
+
+    actualizarTotal();
+}
+//--
+
+//Funcion para sumar los precios del carrito
+function actualizarTotal(){
+  let total = 0;
+  const CarritoTotal = document.querySelector('.shoppingCartTotal');
+  const carritoCartItems = document.querySelectorAll('.shoppingCartItem');
+
+  carritoCartItems.forEach(shoppingCartItem =>{
+
+    const shoppingCartItemPriceElemento = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+    const shoppingCartItemPrecio = Number(shoppingCartItemPriceElemento.textContent.replace('$', ''));
+
+    const carritoCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+    const carritoCartItemQuantity = Number(carritoCartItemQuantityElement.value);
+
+    total = total + shoppingCartItemPrecio * carritoCartItemQuantity;
+
+  });
+  CarritoTotal.innerHTML = `${total}$`
+}
+//--
+
+//Funcion para remover productos del carrito
+function removecarrito(e){
+  const buttonclick = e.target;
+  buttonclick.closest('.shoppingCartItem').remove();
+  actualizarTotal();
+}
+//--
+
+//Funcion para aumentar la cantidad de productos
+function Quantitycambio(e){
+  const cam = e.target;
+  if(cam.value <= 0){
+    cam.value = 1;
+  }
+  actualizarTotal();
+}
+//--
+
+//Funcion de compra en el carrito
+function comprarButtonClick() {
+  carritoCartCONTAINER.innerHTML = '';
+  actualizarTotal();
+}
+//--
